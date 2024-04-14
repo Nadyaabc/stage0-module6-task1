607057881db1896fba55f5136c6aa23c6e35b983
Nadejda <nadyash295923@gmail.com>
2024-04-14 14:17:39 +0300

    done Animal

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,25 @@
 package com.epam.mjc.stage0;
-
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public Animal(String color, int numberOfPaws, boolean hasFur)
+    {}
+    public String getDescription()
+    {
+        String description = "This animal is mostly " + color + ". It has " + numberOfPaws + " paw";
+        if (numberOfPaws != 1) description += "s ";
+        description += "and ";
+        if (hasFur) description += "a fur.";
+        else description += "no fur.";
+        return description;
+    }
+
 
 }
+/*Add a methods getDescription(),
+
+witch would use class fields and return a string with such pattern
+"This animal is mostly (color). It has (numberOfPaws) paws and ('a'/'no' ->
+depends on value of hasFur) fur."
+ */
